b2e2d19df965d7f77168cf2ba8c5176b6b1fc158
jharshith <jangaharshith.reddy2022@vitstudent.ac.in>
2026-07-14 20:23:37 +0530

    loops_challenge_task

--- PYTHON/loops_task.py
+++ PYTHON/loops_task.py
@@ -174,4 +174,194 @@
 #         print("pin should be 4 digits")
 #         continue
 
-#
+#nested loop
+#task1
+
+# letters=["A","B","C"]
+# numbers=[1,2,3]
+# for letter in letters:
+#     for num in numbers:
+#         print(f"{letter}{num}",end=" ")
+
+#     print()
+
+#task2
+# students= ['s1','s2','s3','s4' ]
+# subject=['sub1','sub2','sub3']
+# for student in students:
+#     for sub in subject:
+#         marks=input(f"enter marks for {student} in {sub} ")
+#         print(f"marks for {student} in {sub} is {marks}")
+
+#task 3
+# employees=['e1','e2','e3','e4','e5']
+# days=['mon','tue','wed','thu','fri','sat','sun']
+# for employee in employees:
+#     for day in days:
+#         attendence=input(f"enter attendence for {employee} on {day} : ")
+#         print(f"attendence for {employee} on {day} is {attendence} : ")
+
+#task 4
+# servers=["web1", "web2", "db1"]
+# services= ["Apache", "Docker", "Nginx"]
+# for i in servers:
+#     for j in services:
+#         print(f" server {i} has service {j}")
+
+#task 5
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i*j,end=" ")
+#     print()
+
+#adv nested loop
+#task 1
+# customers=['c1','c2','c3']
+# products=['p1','p2','p3','p4','p5']
+
+# for i in customers:
+#     sum=0
+#     for j in products:
+#         bill=int(input(f"enter bill for the customer {i} for the product {j} : "))
+#         sum+=bill
+#     print(f"total bill for the customer {i} is {sum}")
+
+#task 2
+# customers=['c1','c2','c3','c4','c5']
+# #10 transactions each customer
+# transactions=[100,200,300,400,500,600,700,800,900,1000]
+
+# for i in customers:
+#     for j in transactions:
+#         print(f"customer {i} has transaction amount {j}")
+
+# print("================================")
+
+#task 3
+# lines=[["info"]*99+["error"] for _ in range(10)]
+# for line in lines:
+#     print(line)
+
+# print("================================")
+
+# Correct answers for 10 questions
+# correct_answers = ["A", "B", "C", "D", "A", "B", "C", "D", "A", "B"]
+
+# students = []
+
+# Get answers from 5 students
+# for i in range(5):
+#     print("\nStudent", i + 1)
+
+#     answers = []
+
+#     # Get 10 answers
+#     for j in range(10):
+#         ans = input(f"Question {j + 1} Answer (A/B/C/D): ").upper()
+#         answers.append(ans)
+
+#     students.append(answers)
+
+# # Evaluate scores
+# print("\nResults")
+# for i in range(5):
+#     score = 0
+
+#     for j in range(10):
+#         if students[i][j] == correct_answers[j]:
+#             score += 1
+
+#     print("Student", i + 1, "Score:", score, "/10")
+
+
+#task 5 lms project
+# students = ["Rahul", "Priya", "Amit"]
+# courses = ["Python", "Java", "SQL", "AI"]
+
+# # Store registered courses for each student
+# registered = []
+
+# for student in students:
+#     print("\nStudent:", student)
+
+#     student_courses = []
+
+#     for course in courses:
+#         choice = input(f"Register for {course}? (yes/no): ")
+
+#         if choice.lower() == "yes":
+#             student_courses.append(course)
+
+#     registered.append(student_courses)
+
+# # Display Report
+# print("\n----- LMS Report -----")
+
+# for i in range(len(students)):
+#     print("\nStudent:", students[i])
+#     print("Courses:", registered[i])
+#     print("Course Count:", len(registered[i]))
+
+#ATM
+# Login Details
+username = "admin"
+password = "1234"
+
+# Account Details
+balance = 1000
+transactions = []
+
+# Login
+user = input("Enter Username: ")
+pwd = input("Enter Password: ")
+
+if user == username and pwd == password:
+    print("\nLogin Successful!")
+
+    while True:
+        print("\n===== ATM MENU =====")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Balance Check")
+        print("4. Transaction History")
+        print("5. Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            amount = int(input("Enter deposit amount: "))
+            balance += amount
+            transactions.append("Deposited ₹" + str(amount))
+            print("Deposit Successful!")
+
+        elif choice == 2:
+            amount = int(input("Enter withdrawal amount: "))
+
+            if amount <= balance:
+                balance -= amount
+                transactions.append("Withdrawn ₹" + str(amount))
+                print("Withdrawal Successful!")
+            else:
+                print("Insufficient Balance!")
+
+        elif choice == 3:
+            print("Current Balance: ₹", balance)
+
+        elif choice == 4:
+            print("\nTransaction History")
+
+            if len(transactions) == 0:
+                print("No Transactions Yet.")
+            else:
+                for transaction in transactions:
+                    print(transaction)
+
+        elif choice == 5:
+            print("Thank you for using the ATM!")
+            break
+
+        else:
+            print("Invalid Choice!")
+
+else:
+    print("Invalid Username or Password!")
